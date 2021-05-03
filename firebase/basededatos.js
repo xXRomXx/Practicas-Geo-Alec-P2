8e778040e3fbf8c76c336033d22bb46d79df0feb
Alec Coria <alec_coria@hotmail.com>
2021-05-03 14:11:52 -0500

    practica1

--- firebase/basededatos.js
+++ firebase/basededatos.js
@@ -1,12 +1,11 @@
-    var firebaseConfig = {
-        apiKey: "AIzaSyCrXFL_mMbyLRVSC9Fn6_lfsDjMx54EzlE",
-        authDomain: "proyecto1-935aa.firebaseapp.com",
-        databaseURL: "https://proyecto1-935aa.firebaseio.com",
-        projectId: "proyecto1-935aa",
-        storageBucket: "proyecto1-935aa.appspot.com",
-        messagingSenderId: "514873387506",
-        appId: "1:514873387506:web:fa75cac073ad42be0b636b"
-    };
-
-    firebase.initializeApp(firebaseConfig);
-    const db = firebase.firestore();
+var firebaseConfig = {
+    apiKey: "AIzaSyDDWH-aXlRF-yctZ8OdVF1WulxK1kXlriw",
+    authDomain: "alecgeo2p.firebaseapp.com",
+    projectId: "alecgeo2p",
+    storageBucket: "alecgeo2p.appspot.com",
+    messagingSenderId: "754534089604",
+    appId: "1:754534089604:web:b10cc5fe3c7e476d4053d1",
+    measurementId: "G-KWZFYWBP9K"
+  };
+firebase.initializeApp(firebaseConfig);
+const db = firebase.firestore();
